c9a51b4f3e885e57694bb48c1dccf8c2b7ec7417
webstar9 <pweb047@aucklanduni.ac.nz>
2016-10-12 21:08:27 +1300

    Basic Scene Setup
    Added Walls, Floors, Light, 1st Person Player

---
+++ Assets/Scripts/MovingBird.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingBird : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    public float amplitude;
+
+    private Vector3 tempPosition;
+
+	// Use this for initialization
+	void Start () {
+        tempPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        tempPosition.x += horizontalSpeed;
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+        transform.position = tempPosition;
+	}
+}
