5eddd0086b21e173642c5937b35004a083f7e58d
webstar9 <pweb047@aucklanduni.ac.nz>
2016-10-13 22:38:44 +1300

    Added Marae Image for Maori section
    This hides and shows again based on the button click.

---
+++ Assets/Scripts/MaraeScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaraeScript : MonoBehaviour {
+
+    public Renderer rend;
+    public GameObject player;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        player = GameObject.FindWithTag("Player");
+
+    }
+
+
+    void Update()
+    {
+        if (player.GetComponent<Inventories>().pressedGFButton == true)
+            {
+            this.rend.enabled = true;
+            }
+        else if (player.GetComponent<Inventories>().pressedGFButton == false)
+        {
+            // hide
+            this.rend.enabled = false;
+        }
+    }
+}
