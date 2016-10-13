66baad0d823f236bfb65fc5df683e781cb9d6d79
webstar9 <pweb047@aucklanduni.ac.nz>
2016-10-13 17:23:00 +1300

    Added Soldier Prefab and GameObject
    Created SoldierScript, and Sketch2 script.

--- Assets/Scripts/Sketch2.cs
+++ Assets/Scripts/Sketch2.cs
@@ -1,12 +1,15 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using Pathfinding.Serialization.JsonFx; //make sure you include this using
 
-public class Sketch : MonoBehaviour {
+
+public class Sketch2 : MonoBehaviour {
     public GameObject myPrefab;
     // Put your URL here
     public string _WebsiteURL = "http://infosys320pweb047.azurewebsites.net/tables/AMBirds?ZUMO-API-VERSION=2.0.0";
 
-    void Start () {
+    void Start()
+    {
         //Reguest.GET can be called passing in your ODATA url as a string in the form:
         //http://{Your Site Name}.azurewebsites.net/tables/{Your Table Name}?zumo-api-version=2.0.0
         //The response produce is a JSON string
@@ -21,28 +24,24 @@ public class Sketch : MonoBehaviour {
         //We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
         Product[] products = JsonReader.Deserialize<Product[]>(jsonResponse);
 
-        int totalBirds = products.Length;
-        int totalBirdDistance = 20;
+        int totalSoldiers = products.Length;
         int i = 0;
         //We can now loop through the array of objects and access each object individually
         foreach (Product product in products)
         {
             //Example of how to use the object
-            Debug.Log("This bird's name is: " + product.CommonName);
-            float perc = i / (float)totalBirds;
+            Debug.Log("This soldier's name is: " + product.CommonName);
             i++;
-            float x = perc * totalBirdDistance;
-            float y = 16.0f;
-            float z = 0.0f;
-            GameObject newBird = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
-            newBird.GetComponent<BirdScript>().SetSize(1.0f);
-            newBird.GetComponent<BirdScript>().rotateSpeedBird = perc;
-            newBird.GetComponentInChildren<TextMesh>().text = product.CommonName;
+            float x = Random.Range(-12.0F, 12.0F);
+            float y = 4.0f;
+            float z = Random.Range(-12.0F, 12.0F);
+            GameObject newSoldier = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
---
+++ Assets/Scripts/SoldierScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoldierScript : MonoBehaviour
+{
+    public Vector3 soldierLocation = new Vector3(0, 0, 0);
+
+    void Start()
+    {
+        soldierLocation = new Vector3(Random.value, Random.value, Random.value);
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
